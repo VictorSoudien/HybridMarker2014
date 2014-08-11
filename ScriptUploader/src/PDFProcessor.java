11964ba04f7bc381c6a63007a1bbcae122daa2fd
VictorSoudien <sdnvic001@myuct.ac.za>
2014-08-11 22:25:34 +0200

    Added Tess4j library

--- ScriptUploader/src/PDFProcessor.java
+++ ScriptUploader/src/PDFProcessor.java
@@ -4,7 +4,24 @@
  * Student Number: SDNVIC001
  */
 
+import java.io.*;
+import net.sourceforge.tess4j.*;
+
 public class PDFProcessor 
 {
+	public static void main(String[] args) 
+	{
+		File imageFile = new File("Golden_Globe_text_logo.png");
+		Tesseract instance = Tesseract.getInstance();
 
+		try
+		{
+			String result = instance.doOCR(imageFile);
+			System.out.println(result);
+		} 
+		catch (TesseractException e) 
+		{
+			System.err.println(e.getMessage());
+		}
+	}
 }
