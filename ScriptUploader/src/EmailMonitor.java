82962cae42678d685fbc6b5b1037ee76f2f2b337
VictorSoudien <sdnvic001@myuct.ac.za>
2014-08-11 17:46:44 +0200

    Added javaMail library

---
+++ ScriptUploader/src/EmailMonitor.java
@@ -0,0 +1,10 @@
+/* Monitors an email inbox so that emailed scripts can be processed
+ * Author: Victor Soudien
+ * Date: 11 August 2014
+ * Student Number: SDNVIC001
+ */
+
+public class EmailMonitor 
+{
+
+}
