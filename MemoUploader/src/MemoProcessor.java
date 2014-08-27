cd805dd9a9d60aea75c758f0f3cefa74ce502933
VictorSoudien <sdnvic001@myuct.ac.za>
2014-08-27 13:31:46 +0200

    [BackEnd][MemoUploader] Load file into memory
    - Added test file
    - Implement code to open file

--- MemoUploader/src/MemoProcessor.java
+++ MemoUploader/src/MemoProcessor.java
@@ -4,10 +4,34 @@
  * Student Number: SDNVIC001
  */
 
-
-
+import java.io.File;
 
 public class MemoProcessor 
 {
-
+	private File memoToProcess;
+	
+	public MemoProcessor(String filename)
+	{
+		openFile(filename);
+	}
+	
+	// Loads the file into memory
+	private void openFile(String filename)
+	{
+		memoToProcess = new File(filename);
+		
+		// Check if the file exists
+		if (!memoToProcess.exists())
+		{
+			System.out.println ("Unable to open file: " + filename);
+			System.exit(0);
+		}
+		
+		System.out.println ("File opened successfully");
+	}
+	
+	public static void main (String [] args)
+	{
+		MemoProcessor mProc = new MemoProcessor("ClassTest4.pdf");
+	}
 }
