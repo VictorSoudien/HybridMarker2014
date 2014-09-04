39aebbcae54a90c584042bef38d49e059a0ff41d
VictorSoudien <sdnvic001@myuct.ac.za>
2014-09-04 22:10:51 +0200

    [FrontEnd] Implemented Undo

--- TabletInterface/gen/com/VictorZahraa/hybridmarker/R.java
+++ TabletInterface/gen/com/VictorZahraa/hybridmarker/R.java
@@ -22,16 +22,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int page1200dpi=0x7f020001;
-        public static final int page2200dpi=0x7f020002;
-        public static final int page3200dpi=0x7f020003;
-        public static final int page4200dpi=0x7f020004;
-        public static final int page5200dpi=0x7f020005;
-        public static final int warlock=0x7f020006;
+        public static final int ic_action_undo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int page1200dpi=0x7f020002;
+        public static final int page2200dpi=0x7f020003;
+        public static final int page3200dpi=0x7f020004;
+        public static final int page4200dpi=0x7f020005;
+        public static final int page5200dpi=0x7f020006;
+        public static final int warlock=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f09000e;
+        public static final int action_undo=0x7f09000f;
         public static final int answerText=0x7f090004;
         public static final int answerTextScrollView=0x7f090003;
         public static final int button_recog_gesture=0x7f09000b;
@@ -40,10 +42,10 @@ public final class R {
         public static final int container=0x7f090000;
         public static final int imageView1=0x7f09000d;
         public static final int markingScreenCanvasContainer=0x7f090008;
-        public static final int menu_option_Eraser=0x7f090010;
-        public static final int menu_option_Pen=0x7f09000f;
-        public static final int menu_option_recog_gesture=0x7f090011;
-        public static final int menu_option_view_pdf=0x7f090012;
+        public static final int menu_option_Eraser=0x7f090011;
+        public static final int menu_option_Pen=0x7f090010;
+        public static final int menu_option_recog_gesture=0x7f090012;
+        public static final int menu_option_view_pdf=0x7f090013;
         public static final int questionText=0x7f090002;
         public static final int questionTextScrollView=0x7f090001;
         public static final int scriptDisplay=0x7f090006;
@@ -68,25 +70,13 @@ public final class R {
         public static final int marking_gesture_data=0x7f040000;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f06000d;
         public static final int action_settings=0x7f060002;
-        public static final int action_sign_in_register=0x7f06000b;
-        public static final int action_sign_in_short=0x7f06000c;
+        public static final int action_undo=0x7f060008;
         public static final int app_name=0x7f060000;
-        public static final int error_field_required=0x7f060012;
-        public static final int error_incorrect_password=0x7f060011;
-        public static final int error_invalid_email=0x7f06000f;
-        public static final int error_invalid_password=0x7f060010;
         public static final int hello_world=0x7f060001;
-        public static final int login_progress_signing_in=0x7f06000e;
         public static final int menu_option_Eraser=0x7f060004;
         public static final int menu_option_Pen=0x7f060003;
         public static final int menu_option_recog_gesture=0x7f060005;
-        /**  Strings related to login 
-         */
-        public static final int prompt_email=0x7f060009;
-        public static final int prompt_password=0x7f06000a;
-        public static final int title_activity_login_activity_screen=0x7f060008;
         public static final int title_activity_main_marking_screen=0x7f060007;
         public static final int title_activity_pdf_display_screen=0x7f060006;
     }
--- TabletInterface/src/com/VictorZahraa/hybridmarker/MainMarkingScreenActivity.java
+++ TabletInterface/src/com/VictorZahraa/hybridmarker/MainMarkingScreenActivity.java
@@ -350,8 +350,13 @@ public class MainMarkingScreenActivity extends Activity implements ActionBar.Tab
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings) 
+		{
 			return true;
+		}
+		else if (id == R.id.action_undo)
+		{
+			sCanvasView.undo();
 		}
 		return super.onOptionsItemSelected(item);
 	}
