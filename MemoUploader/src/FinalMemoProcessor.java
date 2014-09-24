5d72533d9c3cb2462f41c1a7347f0c6ded30ffe0
VictorSoudien <sdnvic001@myuct.ac.za>
2014-09-24 13:13:49 +0200

    [BackEnd][MemoProcessor] Multiple changes
    - Implemented image processing approach
    - Calculate total for the test
    - Write the metadata to the file

--- MemoUploader/src/FinalMemoProcessor.java
+++ MemoUploader/src/FinalMemoProcessor.java
@@ -4,55 +4,299 @@
  * Student Number: SDNVIC001
  */
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.util.PDFTextStripper;
+import org.opencv.core.Core;
+
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 
 public class FinalMemoProcessor 
 {
 	private File memoFile;
 	private File blankScriptFile;
-	
+
 	// The name of the output file
-	private String ouptutFileName;
-	
+	private String outputFileName;
+
 	// Stores the answer regions in terms of their start and end y position
 	private ArrayList<String> answerRegions;
-	
+
+	// Keeps track of which question a set of sub-questions and answers belongs to
+	private ArrayList<Integer> mainQuestionIndex;
+
+	private ArrayList<String> mainQuestions;
+	private ArrayList<String> subQuestions;
+	private ArrayList<String> answers;
+
+	// The strings which will be written to the file
+	private String outputHeader;
+
 	public FinalMemoProcessor (String memoFileName, String blankScriptFileName)
 	{
-		ouptutFileName = memoFileName.split("\\.")[0].replaceAll(" ", "_");
-		ouptutFileName += ".txt";
+		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+
+		outputFileName = memoFileName.split("\\.")[0].replaceAll(" ", "_");
+		outputFileName += ".txt";
 		
+		outputHeader = "";
+
+		answerRegions = new ArrayList<String>();
+		mainQuestionIndex = new ArrayList<Integer>();
+		mainQuestions = new ArrayList<String>();
+		subQuestions = new ArrayList<String>();
+		answers = new ArrayList<String>();
+
 		openFiles(memoFileName, blankScriptFileName);
+		getAnswerRegions();
+		getMemoText();
 	}
-	
+
 	// Load the files that need to be processed into memory
 	private void openFiles (String memoFileName, String blankScriptFileName)
 	{
 		memoFile = new File(memoFileName);
 		blankScriptFile = new File(blankScriptFileName);
-		
+
 		// Check if the file exists
 		if (!memoFile.exists())
 		{
 			System.out.println ("Unable to open file: " + memoFileName);
 			System.exit(0);
 		}
-		
+
 		// Check if the file exists
 		if (!blankScriptFile.exists())
 		{
 			System.out.println ("Unable to open file: " + blankScriptFileName);
 			System.exit(0);
 		}
-		
+
 		System.out.println ("Files opened successfully");
 	}
 
 	// Get the start and end y coordinates of the answer regions
 	private void getAnswerRegions()
 	{
+		try
+		{
+			PDDocument blankScript = PDDocument.load(blankScriptFile);
+			List<PDPage> pages = blankScript.getDocumentCatalog().getAllPages();
+
+			HorizontalLineDetection lineDetector = new HorizontalLineDetection();
+
+			// Process each page and get the answer regions
+			for (int i = 1; i < pages.size(); i++)
+			{
+				File output = new File("temp/page.png");
+				ImageIO.write(pages.get(i).convertToImage(), "png", output);
+
+				ArrayList<String> result = lineDetector.processImage("temp/page.png");
+				output.delete();
+
+				for (String line : result)
+				{
+					answerRegions.add(line);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println ("An error occured while detecting answer regions:\n" + e);
+			System.exit(0);
+		}
+	}
+
+	// Get the text of the memo
+	private void getMemoText()
+	{
+		try
+		{
+			PDDocument memoPDF = PDDocument.load(memoFile);
+			PDFTextStripper txtStripper = new PDFTextStripper();
+			txtStripper.setStartPage(2);
+			String docText = txtStripper.getText(memoPDF);
+
+			processMemoText(docText, txtStripper.getLineSeparator());
+		}
+		catch (Exception e)
+		{
+			System.out.println ("Error while getting memo text:");
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	// Processes the text in order to split it into questions and answers
+	private void processMemoText(String memoText, String lineSeparator)
+	{
+		String [] lines = memoText.split(lineSeparator);
+
+		String currentLine = "";
+		boolean inAnswerSection = false;
+
+		int answerStartIndex = 0;
+		int answerEndIndex = 0;
+
+		int answerCounter = 0;
+
+		// Holds the current unassigned lines. Lines are assigned as being either question or answer sections.
+		String tempSection = "";
+
+		for (int i = 0; i < lines.length; i++)
+		{
+			if (lines[i].equalsIgnoreCase(""))
+			{
+				continue;
+			}
+
+			currentLine = lines[i].trim(); 
+			currentLine += "\n";
+
+			if (currentLine.contains("]$"))
+			{
+				System.out.println (currentLine);
+			}
+
+			if ((currentLine.indexOf("Question") == 0) || (currentLine.indexOf("question") == 0))
+			{
+				mainQuestionIndex.add(subQuestions.size());
+				mainQuestions.add(currentLine.replaceAll("[ ]+", " "));
+
+				// Anything before this can be assumed to be additional information e.g. page numbers
+				tempSection = "";
+
+				// Make sure there are only single spaces in the text
+				outputHeader += currentLine.replaceAll("[ ]+", " ");
+				continue;
+			}
+			else if (inAnswerSection == true)
+			{
+				int indexOfEndMarker = currentLine.indexOf("{end}");
+
+				// Check if this is the last line of the answer
+				if (indexOfEndMarker != -1)
+				{
+					inAnswerSection = false;
+					answerEndIndex = i;
+
+					tempSection += currentLine.substring(0, indexOfEndMarker);
+
+					answerCounter++;
+
+					answers.add(tempSection);
+					tempSection = "";
+				}
+				else
+				{
+					tempSection += currentLine;
+				}
+
+				continue;
+			}
+			else if (currentLine.split("]$").length != 1)
+			{
+				inAnswerSection = true;
+				answerStartIndex = (i + 1);
+
+				tempSection += currentLine;
+				subQuestions.add(tempSection);
+				tempSection = "";
+
+				continue;
+			}
+			else 
+			{
+				tempSection += currentLine;
+			}
+		}
+
+		writeMetaFile();
+	}
+
+	// Write the metadata to the file
+	private void writeMetaFile ()
+	{
+		int lowerBound = 0;
+		int upperBound = 0;
+
+		PrintWriter fileWriter = null;
 		
+		try
+		{
+			fileWriter = new PrintWriter(new File (outputFileName));
+			fileWriter.append(calculateTestTotal() + "\n");
+			fileWriter.append(outputHeader);
+			fileWriter.append("{HeaderEnd}\n");
+
+			for (int i = 0; i < mainQuestions.size(); i++)
+			{
+				//System.out.print (mainQuestions.get(i));
+				fileWriter.append(mainQuestions.get(i));
+
+				if (i != (mainQuestions.size() - 1))
+				{
+					upperBound = mainQuestionIndex.get(i + 1);
+				}
+				else
+				{
+					upperBound = subQuestions.size();
+				}
+
+				for (int iter = lowerBound; iter < upperBound; iter++)
+				{
+					/*System.out.print (subQuestions.get(iter));
+					System.out.println ("{QASplit}");
+					System.out.println (answers.get(iter));
+					System.out.println (answerRegions.get(iter));
+					System.out.println ("{SubQEnd}");*/
+					
+					fileWriter.append (subQuestions.get(iter));
+					fileWriter.append ("{QASplit}\n");
+					fileWriter.append (answers.get(iter) + "\n");
+					fileWriter.append (answerRegions.get(iter) + "\n");
+					fileWriter.append ("{SubQEnd}\n");
+				}
+
+				//System.out.println ("{MainQEnd}");
+				fileWriter.append ("{MainQEnd}\n");
+				lowerBound = upperBound;
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println ("An error occured while writing to the file: ");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		finally
+		{
+			if (fileWriter != null)
+			{
+				fileWriter.close();
+			}
+		}
+	}
+
+	// Calculate the total of the test
+	private int calculateTestTotal()
+	{
+		int total = 0;
+		
+		String [] questions = outputHeader.split("\\n");
+		
+		for (String q : questions)
+		{
+			String marks = q.substring(q.indexOf("[") + 1).split(" ")[0];
+			total += Integer.parseInt(marks);
+		}
+		
+		return total;
 	}
 	
 	public static void main (String [] args)
@@ -63,5 +307,7 @@ public class FinalMemoProcessor
 			System.out.println ("Please provide the name of the memo and test script files");
 			System.exit(0);
 		}
+
+		FinalMemoProcessor memoProc = new FinalMemoProcessor(args[0], args[1]);
 	}
 }
---
+++ MemoUploader/src/HorizontalLineDetection.java
@@ -0,0 +1,120 @@
+/* Used to detect the horizontal lines of an answer region
+ * Author: Victor Soudien
+ * Date: 22 September 2014
+ * Student Number: SDNVIC001
+ */
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.imgproc.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class HorizontalLineDetection 
+{
+	private ArrayList<String> regions;
+	
+	public HorizontalLineDetection()
+	{
+		regions = new ArrayList<String>();
+	}
+	
+	public ArrayList<String> processImage(String filename)
+	{
+		BufferedImage imageToProcess = null;
+		regions.clear();
+		
+		try
+		{
+			File input = new File(filename);
+			imageToProcess = ImageIO.read(input);
+		}
+		catch (Exception e)
+		{
+			System.out.println ("Unable to open file: " + filename);
+		}
+		
+		byte[] imageData = ((DataBufferByte) imageToProcess.getRaster().getDataBuffer()).getData();
+		Mat inputImage = new Mat(imageToProcess.getHeight(), imageToProcess.getWidth(), CvType.CV_8UC3);
+		inputImage.put(0, 0, imageData);
+		
+		Mat procImage = inputImage;
+		
+		// Check if the image has been loaded
+		if (inputImage.get(0, 0) == null)
+		{
+			System.out.println ("ERROR");
+			System.exit(0);
+		}
+		
+		// Convert the image to grayscale
+		Imgproc.cvtColor(inputImage, procImage, Imgproc.COLOR_RGB2GRAY);
+		
+		// Perform Canny edge detection to highlight edges
+		Imgproc.Canny(procImage, procImage, 80, 120);
+		
+		Imgproc.HoughLinesP(procImage, procImage, 1, Math.PI / 2.0, 2, 30, 1);
+		
+		ArrayList <Integer> yPositions = new ArrayList<Integer>();
+		int xStart = -1;
+		int xEnd = -1;
+		
+		for (int x = 0; x < procImage.cols(); x++) 
+	    {
+			double[] vec = procImage.get(0, x);
+	          
+	        Point ptStart = new Point(vec[0], vec[1]);
+	        Point ptEnd = new Point (vec[2], vec[3]);
+		          
+		       // Only detect horizontal lines
+			if (ptStart.y == ptEnd.y)
+			{
+				yPositions.add( (int) (Math.ceil(ptEnd.y)));
+
+				xStart = (int) Math.ceil(Math.max(ptStart.x, xStart)); // Use max to ensure that largest x values are stored
+				xEnd = (int) Math.ceil(Math.max(ptEnd.x, xEnd));
+			}
+	    }
+		
+		// Sort the ArrayList in increasing order
+		Collections.sort(yPositions);
+		
+		Point regionStart = new Point();
+		Point regionEnd = new Point();
+
+		regionStart.x = xStart;
+		regionEnd.x = xEnd;
+
+		regionStart.y = yPositions.get(0) - 70;
+		int prevY = yPositions.get(0);
+
+		for (int iter = 1; iter < yPositions.size(); iter++)
+		{
+			if (yPositions.get(iter) - prevY > 77)
+			{
+				regionEnd.y = prevY + 20;
+
+				regions.add((int) regionStart.y + ";" + (int) regionEnd.y);
+
+				regionStart.y = yPositions.get(iter) - 70;
+			}
+			if (iter == yPositions.size() - 1)
+			{
+				regionEnd.y = yPositions.get(iter) + 20;
+
+				regions.add((int) regionStart.y + ";" + (int) regionEnd.y);
+			}
+
+			prevY = yPositions.get(iter);
+		}
+
+		return regions;
+	}
+}
