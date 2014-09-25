591acb1187ebfa8b77b587973be577925977b54e
VictorSoudien <sdnvic001@myuct.ac.za>
2014-09-25 11:35:56 +0200

    [FrontEnd] Added revised marking screen

---
+++ TabletInterface/src/com/VictorZahraa/hybridmarker/PerQuestionMarkingActivity.java
@@ -0,0 +1,54 @@
+package com.VictorZahraa.hybridmarker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class PerQuestionMarkingActivity extends Activity 
+{	
+	private ScrollView scriptScrollView;
+	private ImageView scriptDisplay;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_per_question_marking);
+		
+		scriptScrollView = (ScrollView) findViewById(R.id.scriptDisplayScrollView);
+		//scriptScrollView.setScrollY(840);
+		
+		scriptDisplay = (ImageView) findViewById(R.id.scriptDisplay);
+		
+		//scriptDisplay.setScrollY(288);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.per_question_marking, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		
+		if (id == R.id.action_settings) 
+		{
+			scriptScrollView.scrollTo(scriptScrollView.getScrollX(), 288);
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+}
