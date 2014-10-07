fbc2690c9104e3174c44216d79bfb1cf7619abdb
VictorSoudien <sdnvic001@myuct.ac.za>
2014-10-07 18:31:42 +0200

    [FinalStage] Added PDFBox library

---
+++ ImageToPDF/src/Driver.java
@@ -0,0 +1,19 @@
+/* Converts a set of images to a single pdf
+ * Author: Victor Soudien
+ * Date: 7 October 2014
+ * Student Number: SDNVIC001
+ */
+
+public class Driver 
+{
+	// Add an image to the PDF
+	public void addImageToPDF (String imageFileName)
+	{
+		
+	}
+	
+	public static void main (String [] args)
+	{
+		
+	}
+}
