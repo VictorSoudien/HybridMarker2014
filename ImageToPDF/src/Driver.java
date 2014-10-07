c6eabce2ce34aaf0a5d801172f57f36ae54fe2fb
VictorSoudien <sdnvic001@myuct.ac.za>
2014-10-07 18:38:58 +0200

    [FinalStage] Added test files

--- ImageToPDF/src/Driver.java
+++ ImageToPDF/src/Driver.java
@@ -1,3 +1,5 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 /* Converts a set of images to a single pdf
  * Author: Victor Soudien
  * Date: 7 October 2014
@@ -6,6 +8,15 @@
 
 public class Driver 
 {
+	private String outputFilename;
+	private PDDocument outputPDF;
+	
+	public Driver(String outFilename)
+	{
+		outputFilename = outFilename;
+		outputPDF = new PDDocument();
+	}
+	
 	// Add an image to the PDF
 	public void addImageToPDF (String imageFileName)
 	{
@@ -14,6 +25,11 @@ public class Driver
 	
 	public static void main (String [] args)
 	{
+		/*if (args.length != 1)
+		{
+			
+		}*/
 		
+		Driver driver = new Driver("Test.pdf");
 	}
 }
